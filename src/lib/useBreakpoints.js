0be2f91d13fc320543ee7d102b8a57fd84373bc3
Andrej Jurkin <andrej.jurkin@gmail.com>
2019-02-22 16:55:16 +0100

    Return a lowest breakpoint by default

--- src/lib/useBreakpoints.js
+++ src/lib/useBreakpoints.js
@@ -4,29 +4,25 @@ import { defaultBreakpoints } from './Breakpoints';
 const useBreakpoints = (breakpoints = defaultBreakpoints) => {
   const windowSize = useWindowSize();
 
-  // Returns a sorted array of [key, value] pairs
+  // A sorted array of [key, value] pairs
   const sortedBreakpoints = Object.keys(breakpoints)
     .map(k => [k, breakpoints[k]])
     .sort((a, b) => b[1] - a[1]);
 
   const getCurrentBreakpoint = () => {
-    // eslint-disable-next-line consistent-return
+    // eslint-disable-next-line consistent-return, no-plusplus
     for (let i = 0; i < sortedBreakpoints.length; i++) {
       const b = sortedBreakpoints[i];
       if (windowSize.innerWidth >= b[1]) {
         return b[0];
       }
     }
-
-    // ScreenWidth is below lowest breakpoint,
-    // so it will still be set to equal lowest breakpoint instead of null
-    return 'undefined';
+    return breakpoints[breakpoints.length - 1][0];
   };
 
   return {
     windowSize,
     breakpoint: getCurrentBreakpoint(),
-    sortedBreakpoints,
   };
 };
 
